291cc9ef9a68c26d9f2c0e8b74730ab83179d682
Elena Uvarova <uvelmi@mail.ru>
2024-02-02 20:48:37 +0700

    оживляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,4 @@
-import { login } from "../api.js";
+import { loginUser } from "../api.js";
 
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
 	const appHtml = `<h1>Список задач</h1>
@@ -11,7 +11,7 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
 		  <br />
 		  <br />
 		  Пароль
-        <input type="text" id="password-input" class="input"/>
+        <input type="password" id="password-input" class="input"/>
       </div>
       <br />
       <button class="button" id="login-button">Войти</button>
@@ -20,13 +20,30 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
       appEl.innerHTML = appHtml;
 
 		document.getElementById('login-button').addEventListener('click', () => {
-			login({
-				login: 'admin',
-				password: 'admin',
+			const login = document.getElementById("login-input").value;
+			const password = document.getElementById("password-input").value;
+
+			if (!login) {
+				alert("Введите логин");
+				return;
+			}
+			
+			if (!password) {
+				alert("Введите пароль");
+				return;
+			}
+
+			loginUser({
+				login: login,
+				password: password,
 			}).then((user) => {
 					// setToken("Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k");
 				setToken(`Bearer ${user.user.token}`);
 				fetchTodosAndRender();
+		})
+		.catch((error) => {
+			//TODO: Выводить алерт красиво
+			alert(error.message);
 		});
 	});
 }
